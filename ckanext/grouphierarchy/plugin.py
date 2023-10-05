5d08e2d7425d416344a2b2b29945c193aff10c11
Marija <93824048+MarijaKnezevic@users.noreply.github.com>
2023-10-05 15:45:28 +0200

    Update for pentest (email number and cross site scripting)

--- ckanext/grouphierarchy/plugin.py
+++ ckanext/grouphierarchy/plugin.py
@@ -1,16 +1,16 @@
 import logging
 
 import ckan.plugins as p
-
 import ckanext.grouphierarchy.cli as cli
 from ckanext.grouphierarchy.logic import action
 from ckanext.grouphierarchy import helpers
+from ckanext.grouphierarchy import middleware
 
 
 log = logging.getLogger(__name__)
 
 # This plugin is designed to work only these versions of CKAN
-p.toolkit.check_ckan_version(min_version='2.0')
+p.toolkit.check_ckan_version(min_version="2.0")
 
 
 class HierarchySDDIDisplay(p.SingletonPlugin):
@@ -18,16 +18,16 @@ class HierarchySDDIDisplay(p.SingletonPlugin):
     p.implements(p.IActions, inherit=True)
     p.implements(p.ITemplateHelpers, inherit=True)
     p.implements(p.IPackageController, inherit=True)
+    p.implements(p.IMiddleware, inherit=True)
     p.implements(p.IClick)
-
 
     # IConfigurer
 
     def update_config(self, config_):
-        p.toolkit.add_template_directory(config_, 'templates')
-        p.toolkit.add_public_directory(config_, 'public')
-        p.toolkit.add_resource('public/scripts/vendor/jstree', 'jstree')
-        p.toolkit.add_resource('fanstatic', 'grouphierarchy')
+        p.toolkit.add_template_directory(config_, "templates")
+        p.toolkit.add_public_directory(config_, "public")
+        p.toolkit.add_resource("public/scripts/vendor/jstree", "jstree")
+        p.toolkit.add_resource("fanstatic", "grouphierarchy")
 
     # IActions
 
@@ -35,20 +35,8 @@ class HierarchySDDIDisplay(p.SingletonPlugin):
         return {
             "group_tree_children_g": action.group_tree_children_g,
             "user_create": action.user_create,
-            'config_option_update': action.config_option_update
+            "user_update": action.user_update,
         }
-
-    def update_config_schema(self, schema):
-        ignore_missing = p.toolkit.get_validator(u'ignore_missing')
-        unicode_safe = p.toolkit.get_validator(u'unicode_safe')
-
-        schema.update({
-            u'ckan.site_intro_paragraph': [ignore_missing, unicode_safe],
-            u'ckan.background_image': [ignore_missing, unicode_safe],
-            u'image_upload': [ignore_missing, unicode_safe],
-            u'clear_image_upload': [ignore_missing, unicode_safe],
-        })
-        return schema
 
     # ITemplateHelpers
 
@@ -59,10 +47,16 @@ class HierarchySDDIDisplay(p.SingletonPlugin):
             "get_group_image": helpers.get_group_image,
             "group_tree_crumbs": helpers.group_tree_crumbs,
             "group_tree_section_g": helpers.group_tree_section_g,
-            "get_recently_modified_group": helpers.get_recently_modified_group
+            "get_recently_modified_group": helpers.get_recently_modified_group,
         }
 
     # IClick
 
     def get_commands(self):
         return cli.get_commands()
+
+    # IMiddleware
+
+    def make_middleware(self, app, config):
+        app.before_request(middleware.ckanext_before_request)
+        return app
